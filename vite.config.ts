b00be52dcdea155283322ad912d1ee277101baeb
Franz Badenhorst <franz@sigsolutions.co.za>
2025-06-10 12:17:22 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,8 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/jacaranda-ad-boost-66/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash].mjs',
-        chunkFileNames: 'assets/[name]-[hash].mjs',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      },
-    },
-  },
-}));
+export default defineConfig({
+  base: '/jacaranda-ad-boost-66/', // replace with your repo name!
+  plugins: [react()],
+})
